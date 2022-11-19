9a575987d11fb70b43c64398b8e6e498ff07671d
João Jorge <52219312+JoaoJorgeEF@users.noreply.github.com>
2022-11-19 10:51:33 -0300

    metodo para listar imoveis de usuario

--- src/main/java/br/com/molar/produtos/repository/ImovelOfertadoRepository.java
+++ src/main/java/br/com/molar/produtos/repository/ImovelOfertadoRepository.java
@@ -2,8 +2,14 @@ package br.com.molar.produtos.repository;
 
 import br.com.molar.produtos.entities.ImovelOfertado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImovelOfertadoRepository extends JpaRepository<ImovelOfertado, Long> {
+
+    @Query(value = "select * from imoveis_ofertados idj where idj.usuario_id = :idUsuario", nativeQuery = true)
+    List<ImovelOfertado> findByIdUsuario(long idUsuario);
 }
