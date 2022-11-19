adda1d047f5584b41e655ac82e1471595444a70a
João Jorge <52219312+JoaoJorgeEF@users.noreply.github.com>
2022-11-19 16:31:03 -0300

    usando openfeign em imoveldesejado tbm

--- src/main/java/br/com/molar/produtos/services/ImovelDesejadoService.java
+++ src/main/java/br/com/molar/produtos/services/ImovelDesejadoService.java
@@ -1,7 +1,10 @@
 package br.com.molar.produtos.services;
 
 import br.com.molar.produtos.entities.ImovelDesejado;
+import br.com.molar.produtos.entities.ImovelOfertado;
+import br.com.molar.produtos.entities.Usuario;
 import br.com.molar.produtos.repository.ImovelDesejadoRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -19,26 +22,58 @@ public class ImovelDesejadoService {
     @Autowired
     private ImovelDesejadoRepository repository;
 
+    @Autowired
+    private UsuarioClient usuarioClient;
+
     @CachePut(value = "imoveisDesejados", key = "#imovel.id")
-    public ImovelDesejado cadastrar(ImovelDesejado imovel){
-       return repository.save(imovel);
+    public ImovelDesejado cadastrar(ImovelDesejado imovel) throws Exception {
+        Usuario usuario = usuarioClient.buscarUsuario(imovel.getUsuario_id());
+        if (usuario == null) throw new Exception("Não foi encontrado usuário com este Id");
+        imovel.setUsuario(usuario);
+        imovel.setUsuario_id(usuario.getId());
+
+        return repository.save(imovel);
     }
 
     @Cacheable(value = "imoveisDesejados", key = "#id")
-    public ImovelDesejado consultar(long id){
-        return repository.findById(id).stream().findFirst().orElse(null);
+    public ImovelDesejado consultar(long id) throws Exception{
+        ImovelDesejado imovel = repository.findById(id).stream().findFirst().orElse(null);
+        if (imovel != null){
+            Usuario usuario = usuarioClient.buscarUsuario(imovel.getUsuario_id());
+            if (usuario == null) throw new Exception("Não foi encontrado usuário com este Id");
+            imovel.setUsuario(usuario);
+        }
+        return imovel;
     }
 
     @Cacheable(value = "imoveisDesejados")
     public List<ImovelDesejado> listar(){
-        return repository.findAll();
+        List<ImovelDesejado> imoveis = repository.findAll();
+        for (ImovelDesejado imovel : imoveis) {
+            Usuario usuario = usuarioClient.buscarUsuario(imovel.getUsuario_id());
+            if (usuario != null){
+                imovel.setUsuario(usuario);
+            }
+        }
+
+        return imoveis;
     }
 
     @CacheEvict(value = "imoveisDesejados", allEntries = true)
-    public void delete(long id) {repository.deleteById(id);}
+    public boolean delete(long id) throws Exception {
+        if (id <= 0) throw new Exception("Id informado não existe");
+        if (!repository.existsById(id)) throw new ObjectNotFoundException(Long.class, "Objeto com Id informado não encontrado na base");
+
+        repository.deleteById(id);
+
+        return !repository.existsById(id);
+    }
 
     @CachePut(value = "imoveisDesejados", key = "#id")
-    public ResponseEntity<ImovelDesejado> atualizarImovelDesejado(long id, ImovelDesejado imovel2) {
+    public ImovelDesejado atualizarImovelDesejado(long id, ImovelDesejado imovel2) throws Exception {
+
+        Usuario usuario = usuarioClient.buscarUsuario(imovel2.getUsuario_id());
+        if (usuario == null) throw new Exception("Não foi encontrado usuário com este Id");
 
         Optional<ImovelDesejado> imovel = repository.findById(id);
         if(imovel.isPresent()){
@@ -50,19 +85,40 @@ public class ImovelDesejadoService {
             imovelDesejado.setNumeroBanheiros(imovel2.getNumeroBanheiros());
             imovelDesejado.setNumeroVagasGaragem(imovel2.getNumeroVagasGaragem());
             imovelDesejado.setPreco(imovel2.getPreco());
-            imovelDesejado.setUsuario_id(imovel2.getUsuario_id());
-            repository.save(imovelDesejado);
-            return new ResponseEntity<>(imovelDesejado, HttpStatus.OK);
+            imovelDesejado.setUsuario_id(usuario.getId());
+            imovelDesejado.setUsuario(usuario);
+            imovelDesejado = repository.save(imovelDesejado);
+
+            return imovelDesejado;
         }
         else
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return null;
 
     }
 
-    public List<ImovelDesejado> listarImoveisDoUsuario(int idUsuario) {return repository.findByIdUsuario(idUsuario);}
+    public List<ImovelDesejado> listarImoveisDoUsuario(int idUsuario) throws Exception {
+        Usuario usuario = usuarioClient.buscarUsuario(idUsuario);
+        if (usuario == null) throw new Exception("Não foi encontrado usuário com este Id");
 
-    public ImovelDesejado consultarImoveldoUsuario(int idUsuario, long id){
-        return repository.findByIdUsuarioEIdImovel(idUsuario, id);
+        List<ImovelDesejado> imoveis = repository.findByIdUsuario(usuario.getId());
+        if (imoveis != null) {
+            for (ImovelDesejado imovel : imoveis) {
+                imovel.setUsuario(usuario);
+            }
+        }
+
+        return imoveis;
+    }
+
+    public ImovelDesejado consultarImoveldoUsuario(int idUsuario, long id) throws Exception{
+        Usuario usuario = usuarioClient.buscarUsuario(idUsuario);
+        if (usuario == null) throw new Exception("Não foi encontrado usuário com este Id");
+        ImovelDesejado imovel = repository.findByIdUsuarioEIdImovel(usuario.getId(), id);
+        if (imovel != null){
+            imovel.setUsuario(usuario);
+        }
+
+        return imovel;
     }
 
 }
